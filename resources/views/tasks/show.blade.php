ecbc6bdb292de7b315e9c4309ab7199adb29ee87
Rie <luvrieg@gmail.com>
2017-10-23 08:00:45 +0000

    Kadai commit

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+    <h1>id = {{ $tasks->id }} の詳細ページ</h1>
+    
+    <p>{{ $tasks->content }}</p>
+    
+    {!! link_to_route('tasks.edit', 'このタスクの編集', ['id' => $tasks->id]) !!}
+    
+    {!! Form::model($tasks, ['route' => ['tasks.destroy', $tasks->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+
+@endsection
